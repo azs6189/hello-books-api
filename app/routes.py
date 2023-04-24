ff7e12e8598b931d3165b808c095ec9dd8856a80
Allie Soliz <solizale@gmail.com>
2023-04-24 01:19:50 -0400

    creates a Book class and instantiates multiple instances

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,16 @@
 # Blueprint is a Flask class that provides a pattern for grouping related routes(endpoints)
 from flask import Blueprint
 
-hello_world_bp = Blueprint("hello_world", __name__)
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
 
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
-
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-
-
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+books = [
+    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
