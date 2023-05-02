ff59ba26afe8d98938c324d628c139f68de3b875
Allie Soliz <solizale@gmail.com>
2023-05-02 18:50:50 -0400

    adds comments and adds conditionals to check test_config

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,36 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# This package imports the load_dotenv() method which loads the values from the .env file so that the os module is able to see them
+from dotenv import load_dotenv
+
+# This built-in module provides a way to read environment variables
+import os
+
 # Sets up db and migrate, which are conventional variables that give us access to database operations
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 def create_app(test_config=None):
     """Starting boilerplate code to start a Flask application and configure the database to use SQLAlchemy"""
     app = Flask(__name__)
 
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
     # Configures the app to include two new SQLAlchemy settings
     # This config is set to False to hide a warning about a feature in SQLAlchemy that will not be used
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # This config is set to the connection string for our database, hello_books_development
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # # This config is set to the connection string for our database, hello_books_development
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
 
     # Import models here
     from app.models.book import Book
