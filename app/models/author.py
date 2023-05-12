eed2658847612da3700005990443359ad047f2fe
Allie Soliz <solizale@gmail.com>
2023-05-11 21:58:09 -0700

    establishes a one-to-many relationship with Author model

--- app/models/author.py
+++ app/models/author.py
@@ -2,6 +2,8 @@
 from app import db
 
 
-class Author:
+class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
+    # establishes the parent-child relatipnship
+    books = db.relationship("Book", back_populates="author")
