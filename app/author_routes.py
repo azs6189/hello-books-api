73e909d0409d0fab289927b32e19b7be3a4fa689
Allie Soliz <solizale@gmail.com>
2023-05-11 21:10:04 -0700

    moves authors_bp from routes into author_routes file

--- app/author_routes.py
+++ app/author_routes.py
@@ -1,3 +1,19 @@
 from app import db
+
 from app.models.author import Author
+
 from flask import Blueprint, jsonify, abort, make_response, request
+
+# Instantiates a Blueprint instance
+authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.get_json()
+    new_author = Author(name=request_body["name"],)
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
