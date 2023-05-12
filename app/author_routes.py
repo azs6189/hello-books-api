9b42a0f83c52ff8d4b207b3ffc5eedcdd7c83d73
Allie Soliz <solizale@gmail.com>
2023-05-11 21:10:56 -0700

    adds create_author route

--- app/author_routes.py
+++ app/author_routes.py
@@ -6,3 +6,14 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 # Instantiates a Blueprint instance
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+
+
+@authors_bp.route("", methods=["POST"])
+def create_author():
+    request_body = request.get_json()
+    new_author = Author(name=request_body["name"],)
+
+    db.session.add(new_author)
+    db.session.commit()
+
+    return make_response(jsonify(f"Author {new_author.name} successfully created"), 201)
