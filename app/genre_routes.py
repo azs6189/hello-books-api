12fa6ae20add4e70a12ca48a1bbecb9ceb203b96
Allie Soliz <solizale@gmail.com>
2023-05-15 09:51:04 -0700

    adds create_genre route

--- app/genre_routes.py
+++ app/genre_routes.py
@@ -2,7 +2,19 @@ from app import db
 
 from app.models.author import Author
 from app.models.book import Book
+from app.models.genre import Genre
 from flask import Blueprint, jsonify, abort, make_response, request
 from app.book_routes import validate_model
 # Instantiates a Blueprint instance
 genres_bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
+
+
+@genres_bp.route("", methods=["POST"])
+def create_genre():
+    request_body = request.get_json()
+    new_genre = Genre(name=request_body["name"],)
+
+    db.session.add(new_genre)
+    db.session.commit()
+
+    return make_response(jsonify(f"Genre {new_genre.name} successfully created"), 201)
